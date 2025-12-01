523343b5eed8072a03d4a7f08efc97b1af9d0edd
drfailer <drfailer.42@gmail.com>
2025-12-01 15:26:01 -0500

    update build script

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-odin build src -extra-linker-flags:"-L$HOME/Programming/usr/lib -Wl,-rpath=$HOME/Programming/usr/lib" -out:tracer_ui -debug -collection:deps=../sgui/
+odin build src \
+    -out:tracer_ui -debug \
+    -collection:deps=../sgui/ \
+    -extra-linker-flags:"-L$HOME/Programming/usr/lib -Wl,-rpath=$HOME/Programming/usr/lib" \
+    -define:FONT="/usr/share/fonts/truetype/jetbrains-mono/JetBrainsMono-Regular.ttf"
