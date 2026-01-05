f62a9f7c71394f4967099324451a3c63ff44bd01
drfailer <drfailer.42@gmail.com>
2026-01-05 19:19:50 +0100

    package the fonts

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ odin build src \
     -out:tracer_ui -debug \
     -collection:deps=../sgui/ \
     -extra-linker-flags:"-L$HOME/Programming/usr/lib -Wl,-rpath=$HOME/Programming/usr/lib" \
-    -define:FONT="/usr/share/fonts/truetype/jetbrains-mono/JetBrainsMono-Regular.ttf"
+    -define:FONT=$PWD/ressources/fonts/OpenSans-Regular.ttf
